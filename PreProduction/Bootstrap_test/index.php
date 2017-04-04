5b1007a36bd6e9ab236636556379e858a0f6d485
GitVALD <steven.canta@gmail.com>
2017-04-04 19:20:02 +0200

    Folder_Change
    Création de dossier plus précis.
    Ajout de la charte graphique.

---
+++ PreProduction/Bootstrap_test/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+  	<head>
+	  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"">
+		<link rel="stylesheet" href="template.css">
+  	</head>
+	  	<body>
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+			  	<div class="container-fluid">
+			  	<div class="navbar-header">
+			      	<a class="navbar-brand" href="#">Mon beau site</a>
+			  	</div>
+			    <ul class="nav navbar-nav">
+			      <li class="active"> <a href="#">Accueil</a> </li>
+			      <li><a href="#">Liens</a></li>
+			      <li><a href="#">Contact</a></li>
+			      <li><a href="#">A propos</a></li>
+			      <li><a href="#">Galerie</a></li>
+			    </ul>
+			    <form class="navbar-form navbar-right inline-form">
+			      <div class="form-group">
+			        <input type="search" class="input-sm form-control" placeholder="Recherche">
+			        <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+			      </div>
+			    </form>
+			  </div>
+			</nav>
+			<br>
+			<br>
+			<br>
+	    	<div class="container">
+	    		<div class="row">
+	    			<div class="col-lg-4">
+				      	<form class="form-inline">
+						  	<div class="input-group"> 
+						    <span class="input-group-addon">€</span>
+						    <input type="text" class="form-control" style="text-align:right" value="100">
+						    <span class="input-group-addon">.00</span> 
+						  	</div>
+						</form>
+					</div>
+
+					<div class="col-lg-4">
+					  	<ul class="pagination">
+					    	<li><a href="#">&laquo;</a></li>
+					    	<li><a href="#">1</a></li>
+					    	<li><a href="#">2</a></li>
+					    	<li><a href="#">3</a></li>
+					    	<li><a href="#">&raquo;</a></li>
+					  	</ul>
+					</div>					  	
+
+					<div class="col-lg-4">
+						<p><a class="btn btn-info btn-lg" role="button">En savoir plus  <span class="glyphicon glyphicon glyphicon-hand-right"></span></a></p>
+					</div>
+				</div>
+
+				<div class="row">
+					<span class="label label-default">Valeur d'inhibition</span> 600
+					<span class="label label-info">Valeur de signalement</span> 4000
+					<span class="label label-success">Valeur de fonctionnement</span> 8000
+					<span class="label label-primary">Valeur optimale</span> 10000
+					<span class="label label-warning">Valeur dangereuse</span> 11000
+					<span class="label label-danger">Valeur critique</span> 12000
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-lg-4">
+						<ul class="list-group">
+						  	<a href="#" class="list-group-item active">
+						    	<span class="glyphicon glyphicon-chevron-right pull-right"></span>
+						      	Item 1
+						  	</a>
+						  	<a href="#" class="list-group-item">
+						    	<span class="glyphicon glyphicon-chevron-right pull-right"></span>
+						      		Item 2
+						  	</a>
+						  	<a href="#" class="list-group-item">
+						    	<span class="glyphicon glyphicon-chevron-right pull-right"></span>
+						      		Item 3
+						  	</a>
+						  	<a href="#" class="list-group-item">
+						    	<span class="glyphicon glyphicon-chevron-right pull-right"></span>
+						      		Item 4
+						  	</a>
+						</ul>
+					</div>
+				</div>
+			</div>
+	    </div>
+	  </body>
+</html>

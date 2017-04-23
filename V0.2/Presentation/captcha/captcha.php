8b877b335eeaded584d65ba25744a485b8ee01f9
GitVALD <steven.canta@gmail.com>
2017-04-23 16:55:08 +0200

    Update Quentin Work
    Update Quentin Work

--- V0.2/Presentation/captcha/captcha.php
+++ V0.2/Presentation/captcha/captcha.php
@@ -19,18 +19,18 @@ $_SESSION['captcha'] = $captcha;
 //$fonts = "fonts/" . $fontsFiles[array_rand($fontsFiles)]; // array_diff permet de retirer le cas où le fichier s'appelle . ou ..
                                                                 // cas possible sur linux par exemple
 
-for ($i=0; $i < $nbChar ; $i++) { 
+for ($i=0; $i < $nbChar ; $i++) {
 	$redTextColor = (~$redBackgroundColor) % 255;		//initialisation des couleurs du texte en prenant
 	$greenTextColor = (~$greenBackgroundColor) % 255;	//l'opposé du background grâce à ~
 	$blueTextColor = (~$blueBackgroundColor) % 255;		//~ permet d'inverser les bits entre 1 et 0 ce qui permet
-														//d'obtenir un très bon contraste 
+														//d'obtenir un très bon contraste
 
 	$textColor = imagecolorallocate($image, $redTextColor, $greenTextColor, $blueTextColor); //soucis: les valeurs sont négatives donc le texte n'a pas d'aliasing
 
 	$textColor = $textColor + 3; //Permet de forcer le typage de la variable $textColor en entier afin que
                                  //valeurs ne soient plus négatives et ainsi reactiver l'antialiasing
 
-	imagettftext($image, rand(25, 50), rand(-20, 20), $i*45+15, rand(70, 90), $textColor, "Calibri.ttf", $captcha[$i]);
+	imagettftext($image, rand(25, 50), rand(-20, 20), $i*45+15, rand(70, 90), $textColor, "captca/Calibri.ttf", $captcha[$i]);
 	imagerectangle($image, rand(0, 300), rand(0, 300), rand(0, 150), rand(0, 150), $textColor);
 	imageellipse($image, rand(0, 300), rand(0, 150), rand(0, 150), rand(0, 150), $textColor);
 }

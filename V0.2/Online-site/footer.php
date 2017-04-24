4157b07ae9262f1b31cf1e4870be9ff9cd7d27c0
zeronakysa <aurelien.delagarde@gmail.com>
2017-04-24 19:36:45 +0200

    Add 1 st SQL script

--- V0.2/Online-site/footer.php
+++ V0.2/Online-site/footer.php
@@ -1,5 +1,34 @@
-<footer>
-  <p>Copyright © 2017 BitNet </p>
-  <p>Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.</p>
+<!-- Footer -->
+<footer id="contact" class="bitnet-footer">
+    <div class="container-fluid">
+        <div class="row footer-main">
+            <div class="col-md-4 contact">
+                <h4>Nous contacter</h4>
+                <form action="mailto:stevencantagrel.contact@gmail.com" method="POST">
+                </form>
+            </div>
+        </div>
+        <div class="row footer-rights">
+            <div class="text-center">
+                <p>© Bitnet, 2017 - Tous droits réservés</p>
+            </div>
+        </div>
+    </div>
+
 </footer>
+
+
+<!-- Bootstrap Jquery Link -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Bootstrap JavaScript Link -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Script Reload Captcha -->
+<script type="text/javascript">
+    $(function() {
+        $('#reload_captcha').click(function(){
+            $('img').attr('src', 'captcha/captcha.php?cache=' + new Date().getTime());
+        });
+    });
+</script>
+</body>
 </html>
--- V0.2/Online-site/index.php
+++ V0.2/Online-site/index.php
@@ -2,6 +2,15 @@
   include "header.php";
   include "navBar.php";
 ?>
+sdfs
+sdf
+sdf
+sdf
+s
+sdf
+sdf
+sdf
+sdf
 <?php
   include "footer.php";
 ?>

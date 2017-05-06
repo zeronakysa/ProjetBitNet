868c7ec9e04cdd023d65b81aaa46144630f5ca34
zeronakysa <aurelien.delagarde@gmail.com>
2017-05-06 17:35:50 +0200

    reparation truc bizarre
    reparation truc bizarre

--- V0.2/Online-site/index.php
+++ V0.2/Online-site/index.php
@@ -1,33 +1,6 @@
 <?php
 		include "header.php";
 	?>
-<<<<<<< HEAD
-	<title>Acceuil</title>
-</head>
-<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
-	<?php
-  		include "navBar.php";
-	?>
-	<br />
-	<br />
-	<br />
-	<h1>
-		jfdgdfg
-		fgdfgdfg
-		fdgdfdf
-		dfgdgdf
-		dfgdfgdf
-		fdgdfg
-		dgf
-	</h1>
-	<?php
-	echo "<br />";
-	print_r($_SESSION);
-	echo "<br />";
-  		include "footer.php";
-	?>
-</body>
-=======
 		<title>Accueil</title>
 	</head>
 	<body>
@@ -44,5 +17,4 @@
 	  		include "footer.php";
 		?>
 	</body>
->>>>>>> refs/remotes/origin/master
 </html>

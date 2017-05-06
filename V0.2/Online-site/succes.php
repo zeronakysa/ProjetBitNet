33b99bde7eecd40f18c3552eb9bc34c018ce69db
GitVALD <steven.canta@gmail.com>
2017-05-06 17:15:55 +0200

    change indent
    change indent

--- V0.2/Online-site/succes.php
+++ V0.2/Online-site/succes.php
@@ -1,27 +1,27 @@
 <?php
 		include "header.php";
 	?>
-	<title>Succes</title>
-</head>
-<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
-	<br />
-	<br />
-	<br />
-	<?php
-  		include "navBar.php";
-			etatSucces();
-			$id_succes = 4;
-			giveSucces($id_succes);
-			$exp = getExp($_SESSION['email']);
-			print_r($_SESSION);
-			print_r($exp);
-	?>
+		<title>Succes</title>
+	</head>
+	<body>
+		<br />
+		<br />
+		<br />
+		<?php
+	  		include "navBar.php";
+				etatSucces();
+				$id_succes = 4;
+				giveSucces($id_succes);
+				$exp = getExp($_SESSION['email']);
+				print_r($_SESSION);
+				print_r($exp);
+		?>
 
-	<p><br>
-		Work In progress (Succes)
-	</p>
-	<?php
-  		include "footer.php";
-	?>
-</body>
+		<p><br>
+			Work In progress (Succes)
+		</p>
+		<?php
+	  		include "footer.php";
+		?>
+	</body>
 </html>

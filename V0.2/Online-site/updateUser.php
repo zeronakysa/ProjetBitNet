3892ba4fb430d610881ff042249eaea7acded1ba
zeronakysa <aurelien.delagarde@gmail.com>
2017-05-07 17:02:54 +0200

    update admin.php
    update admin.php role ne fonctionne pas encore
    update updateUser.php, regarde l'email de la session active et affiche
    les information relatives.

--- V0.2/Online-site/updateUser.php
+++ V0.2/Online-site/updateUser.php
@@ -4,52 +4,43 @@
 	if(!empty($_GET["id"]) && count($_GET)==1 && is_numeric($_GET["id"])){
 	// Récupérer en base de données toutes les informations sur l'user
 		$connection=dbConnect();
-		$query=$connection->prepare("SELECT * FROM USERS WHERE ID_membre=:id");
-		$query->execute($_GET);
-	  $data=$query->fetch();
+		$query=$connection->prepare("SELECT * FROM MEMBRE WHERE email=:email");
+		$query->execute(['email' => $_SESSION['email']]);
+	  $user=$query->fetch();
+
 }
 ?>
 <section>
 	<form method="POST" action="saveUser.php?id=<?php echo $_GET['id']?>">
-		Votre pseudo: <input type="text" name="pseudo" placeholder="<?php echo ($data["pseudo"])?$data["pseudo"]:"";?>" required="required"><br>
-    Votre email: <input type="text" name="pseudo" placeholder="<?php echo ($data["email"])?$data["email"]:"";?>" required="required"><br>
-		<input type="password" name="pwd" placeholder="Votre mot de passe" required="required"><br>
-		<input type="password" name="pwd2" placeholder="Confirmation" required="required"><br>
-		<input type="date" name="birthday" placeholder="Date de naissance"
-		value="<?php echo (isset($data["birthday"]))?$data["birthday"]:"";?>"><br>
+<table>
+		<tr>
+			<th>E-mail </th>
+			<th>Pseudo </th>
+			<th>Nom </th>
+			<th>Prenom </th>
+			<th>Langages </th>
+			<th>Ville </th>
+			<th>Date_naissance </th>
+			<th>Profile_picture </th>
+			<th>Action</th>
+		</tr>
+
 		<?php
-		foreach ($listOfGender as $key => $gender) {
-			echo "<label>";
-			echo $gender;
-			$defaultGender = (isset($data["gender"]) )?$data["gender"]:$defaultGender;
-			if( $key  == $defaultGender){
-				echo "<input type='radio' name='gender' value='".$key."' checked='checked'>";
-			}else{
-				echo "<input type='radio' name='gender' value='".$key."'>";
-			}
-			echo "</label>";
-		}
-		?>
-		<br>
-		<select name="country">
-			<?php
-				foreach ($listOfCountry as $key => $value) {
-					$selected = (
-									isset($data["country"])
-									&&
-									$data["country"]== $key
-								)
-								? "selected='selected'"
-								:"";
-					echo "<option value='".$key."' ".$selected.">".$value."</option>";
-				}
-			?>
-		</select>
-		<br>
-		<textarea name="comment" placeholder="Votre commentaire"><?php echo (isset($data["comment"]))?$data["comment"]:"";?></textarea>
-		<br>
-		<input type="submit" value="S'inscrire">
+		echo "<tr>";
+		echo "<td>".$user["email"]."</td>";?>
+		<td><input value="<?php echo ($user["pseudo"])?$user["pseudo"]:"";?>" type="text" name="pseudo" placeholder="pseudo" required="required"></td>
+		<td><input type="text" name="nom" value="<?php echo ($user["nom"])?$user["nom"]:"";?>" placeholder="Nom"></td>
+		<td><input type="text" name="prenom" value="<?php echo ($user["prenom"])?$user["prenom"]:"";?>" placeholder="Prénom"></td>
+		<td><input type="text" name="langages" value="<?php echo ($user["langages"])?$user["langages"]:"";?>" placeholder="langages"></td>
+		<td><input type="text" name="ville" value="<?php echo ($user["ville"])?$user["ville"]:"";?>" placeholder="Code Postale"></td>
+		<td><input type="date" name="date_naissance" placeholder="Date de naissance" value="<?php echo date('Y-m-d', strtotime($user["date_naissance"]))?>"></td>
+		<td><input type="text" name="profile_picture" value="<?php echo ($user["profile_picture"])?$user["profile_picture"]:"";?>" placeholder="Chemin our URL"></td>
+<!--	 Créer un lien en fin de ligne vers ce fichier
+		 Envoyer en GET l'id de l'user									-->
+		<td><input type="submit" value="Modifier"></td>
+	</tr>
 	</form>
+</table>
 </section>
 <?php
 	include "footer.php";

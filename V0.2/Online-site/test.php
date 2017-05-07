36a2ba3ceaa5327f207a7ee35380767c85ddc4a6
zeronakysa <aurelien.delagarde@gmail.com>
2017-05-07 17:45:10 +0200

    admin.php presque fini
    admin.php presque fini qui renvoie le tout en POST sur test.php
    
    updateUser affiche que selon l'user

---
+++ V0.2/Online-site/test.php
@@ -0,0 +1,24 @@
+<?php
+		include "header.php";
+	?>
+		<title>TEST</title>
+	</head>
+	<body>
+		<?php
+	  		include "navBar.php";
+		?>
+		<br />
+		<br />
+		<br />
+
+		<h1>TEST ZONE </h1>
+
+    <?php
+      print_r ($_POST);
+     ?>
+
+		<?php
+	  		include "footer.php";
+		?>
+	</body>
+</html>

9a2cacddde1ac64d0d0c0a34f5f7629cb3c0f95d
zeronakysa <aurelien.delagarde@gmail.com>
2017-05-22 12:14:59 +0200

    modif aurel
    Add image profile en session
    Une image de profile par defaut a la creation du compte
    test en cours sur projet

---
+++ V0.2/test/projet/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+  </head>
+  <body>
+    <img src="https://i.vimeocdn.com/portrait/58832_300x300" />
+  </body>
+</html>

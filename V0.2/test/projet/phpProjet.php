aff9820dbecc825c5a6c0a8b0e2a5d8d486c6419
zeronakysa <aurelien.delagarde@gmail.com>
2017-05-27 20:50:48 +0200

    Projet in progress
    Changement de la table creation_projet en participe_projet cette table
    permet de savoir qui participe a quelle projet.
    La recherche a faire pour le projet sera me nom createur et le nom du
    projet (pour avoir son ID) et ensuite il faut rajouter, pour cet ID,
    l'email du participant. Ainsi on sait que pour le projet 3 x,y,z
    participe.

--- V0.2/test/projet/phpProjet.php
+++ V0.2/test/projet/phpProjet.php
@@ -2,24 +2,24 @@
 
 session_start();
 
-print_r($_SESSION);
-echo '<br />';
-print_r($_POST);
-$structure = "../PROJETS/".$_SESSION["ID_membre"]."/".$_POST["projectName"]."/";
-$racine = "../PROJETS/".$_SESSION["ID_membre"]."/";
-$root = "../PROJETS/";
-if (file_exists($structure)){
-  echo '<br />Echec lors de la création des répertoires.(Existe déjà)<br />';
-}else{
-  mkdir($structure, 0777, true);
-  echo '<br />Repertoire de projet personnel créer.<br />';
+define("HOST", "localhost");
+define("DBNAME", "projet_bitnet");
+define("DBUSER", "root");
+define("DBPWD", "");
+
+function dbConnect(){
+  //Se connecter à la bdd
+  try{
+      $connection = new PDO("mysql:host=".HOST.";dbname=".DBNAME, DBUSER, DBPWD);
+  }catch(Exception $e){
+      die("Erreur SQL".$e->getMessage());
+  }
+  return $connection;
 }
 
-function listFiles( $from )
-{
+function listFiles( $from ){
     if(! is_dir($from))
         return false;
-
     $files = array();
     $dirs = array( $from);
     while( NULL !== ($dir = array_pop( $dirs)))
@@ -31,30 +31,104 @@ function listFiles( $from )
                 if( $file == '.' || $file == '..')
                     continue;
                 $path = $dir . '/' . $file;
-                if( is_dir($path))
-                    $dirs[] = $path;
-                else
-                    $files[] = $path;
+                if( is_dir($path)){
+                  $dirs[] = $path;
+                  $files[] = $path."/";
+                }else{
+                  $files[] = $path;
+                }
             }
             closedir($dh);
         }
     }
     return $files;
 }
-$test = listFiles($structure);
-echo "<pre>";
-print_r($test);
-echo "</pre>";
-$test2 = listFiles($root);
-echo "<pre>";
-print_r($test2);
-echo "</pre>";
-$test3 = listFiles($racine);
-echo "<pre>";
-print_r($test3);
+
+function listFilesAndPrint( $from )
+{
+  $length = strlen($from);
+    if(! is_dir($from))
+        return false;
+    $dirs = array( $from);
+    while( NULL !== ($dir = array_pop( $dirs)))
+    {
+        if( $dh = opendir($dir))
+        {
+            while( false !== ($file = readdir($dh)))
+            {
+                if( $file == '.' || $file == '..')
+                    continue;
+                $path = $dir . '/' . $file;
+                if( is_dir($path)){
+                  $dirs[] = $path;
+                  $path = substr($path, $length);
+                  echo " ..".$path."/<br />";
+                }else{
+                  $path = substr($path, $length);
+                  echo " ..".$path."<br />";
+                }
+            }
+            closedir($dh);
+        }
+    }
+    return true;
+}
+
+// print_r($_SESSION);
+//echo '<br />';
+//print_r($_POST);
+$structure = "../PROJETS/".$_SESSION["ID_membre"]."/".$_POST["projectName"]."/";
+$racine = "../PROJETS/".$_SESSION["ID_membre"];
+$root = "../PROJETS/";
+
+if (file_exists($structure)){
+  $creationError = 1;
+}else{
+  $connection = dbConnect();
+  $query = $connection->prepare('INSERT INTO `projet` (`nom_projet`, `nom_createur`, `date_creation`, `description_projet`) VALUES (:nom_projet, :nom_createur, NOW(), :description_projet)');
+  $results = $query->execute([
+    'nom_projet'=>$_POST["projectName"],
+    'nom_createur'=>$_SESSION["ID_membre"],
+    'description_projet'=>$_POST["projetDescription"]
+  				]);
+  if($results == 1){
+    mkdir($structure, 0777, true);
+    echo '<br />Projet '.$_POST["projectName"].' créé avec succès.<br />';
+  }else{
+    $creationError = 1
+  }
+  $creationError = 0;
+}
+if ($creationError == 1) {
+
+// A REMETTRE EN LIVE
+//  header('Location: .php?ce=1');
+
+}
+echo "<B>Arborescence personnel</B><pre>";
+listFilesAndPrint($racine);
 echo "</pre>";
 
+echo "<B>Arborescence du projet ".$_POST["projectName"]."</B><pre>";
+listFilesAndPrint($structure);
+echo "</pre>";
+
+$connection = dbConnect();
+$query = $connection->prepare('INSERT INTO `projet` (`nom_projet`, `nom_createur`, `date_creation`, `description_projet`) VALUES (:nom_projet, :nom_createur, NOW(), :description_projet)');
+$results = $query->execute([
+  'nom_projet'=>$_POST["projectName"],
+  'nom_createur'=>$_SESSION["ID_membre"],
+  'description_projet'=>$_POST["projetDescription"]
+				]);
+print_r($results);
 /* OLD
+
+
+$connection = dbConnect();
+$query = $connection->prepare('SELECT * FROM projet WHERE ID_membre = :ID_membre');
+$query->execute(['ID_membre' => $_POST["projectName"]]);
+$results = $query->fetch();
+
 
 //affiche le contenu du dossier du projet
 if (is_dir($structure)) {
@@ -80,6 +154,35 @@ if ($dh = opendir($racine)) {
   }
   closedir($dh);
 }
+}
+
+function listFiles( $from )
+{
+    if(! is_dir($from))
+        return false;
+
+    $files = array();
+    $dirs = array( $from);
+    while( NULL !== ($dir = array_pop( $dirs)))
+    {
+        if( $dh = opendir($dir))
+        {
+            while( false !== ($file = readdir($dh)))
+            {
+                if( $file == '.' || $file == '..')
+                    continue;
+                $path = $dir . '/' . $file;
+                if( is_dir($path)){
+                  $dirs[] = $path;
+                  $files[] = $path."/";
+                }else{
+                  $files[] = $path;
+                }
+            }
+            closedir($dh);
+        }
+    }
+    return $files;
 }
 */
 ?>

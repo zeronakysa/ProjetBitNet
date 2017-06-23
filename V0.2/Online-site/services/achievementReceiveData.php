5e84ce2cbcf8f0993035687fb3d2511367d182d3
GitVALD <steven.canta@gmail.com>
2017-06-23 14:37:49 +0200

    Avancement dans Succès 23-06-17
    Avancement dans Succès 23-06-17

--- V0.2/Online-site/services/achievementReceiveData.php
+++ V0.2/Online-site/services/achievementReceiveData.php
@@ -10,10 +10,10 @@
     // $request = $connection->prepare("SELECT ID_succes FROM `succes`");
 
     // Select in DB All Achievement Done !
-    $request = $connection->prepare("   SELECT  SUCCES.*
+    $request = $connection->prepare("   SELECT  SUCCES.*,succes_reussi.progression
                                         FROM    SUCCES, succes_reussi
                                         WHERE   SUCCES.ID_succes = succes_reussi.ID_succes
-                                        AND     succes_reussi.progression = SUCCES.goal
+                                        AND     succes_reussi.progression <= SUCCES.goal
                                         AND     succes_reussi.email=:email");
 
     $request->execute(["email" => $_SESSION["email"]]);
